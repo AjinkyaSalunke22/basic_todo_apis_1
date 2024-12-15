604bf7b7d13209bdefcf9b62ff760220c285627f
Ajinkya <ajinkya.salunke.official@gmail.com>
2024-12-16 05:28:30 +0530

    User and tasks model created and user controller updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,57 @@
+import Users from "../models/userModel.js";
+
 export const createUserController = (req, res) => {
-  res.send("User created");
+  const { id, name } = req.body;
+
+  if (Users[id]) {
+    return res.status(409).json({ message: "User id already present" });
+  } else {
+    Users[id] = { id, name };
+    return res
+      .status(201)
+      .json({ message: "User created successfully", user: Users[id] });
+  }
 };
 
 export const getUserController = (req, res) => {
-  res.send("Single user");
+  const id = req.params.id;
+
+  if (Users[id]) {
+    return res.status(200).json({ message: "User found", data: Users[id] });
+  } else {
+    return res.status(404).json({ message: "User not found" });
+  }
 };
 
 export const getUsersController = (req, res) => {
-  res.send("Multiple users");
+  if (JSON.stringify(Users) === "{}") {
+    return res.status(404).json({ message: "Users not found" });
+  } else {
+    return res.status(200).json({ message: "Users found", data: Users });
+  }
 };
 
 export const updateUserController = (req, res) => {
-  res.send("Update user");
+  const id = req.params.id;
+  const user = req.body;
+
+  if (Users[id]) {
+    Users[id] = user;
+    return res
+      .status(200)
+      .json({ message: "User updated successfully", data: Users[id] });
+  } else {
+    return res.status(404).json({ message: "User not found" });
+  }
 };
 
 export const deleteUserController = (req, res) => {
-  res.send("User deleted");
+  const id = req.params.id;
+
+  if (Users[id]) {
+    delete Users[id];
+    return res.status(200).json({ message: "User deleted successfully" });
+  } else {
+    return res.status(404).json({ message: "User not found" });
+  }
 };
